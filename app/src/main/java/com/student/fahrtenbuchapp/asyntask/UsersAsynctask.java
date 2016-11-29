a14306dd23d1910f32b03dcebcaf34b77570c9cc
Mojdeh.aaz <mojdeh.aaz@gmail.com>
2016-11-29 15:10:52 +0100

    AsyncTasks und Datenbanken wurden separat erstellt und müssen noch bearbeitet werden.

---
+++ app/src/main/java/com/student/fahrtenbuchapp/asyntask/UsersAsynctask.java
@@ -0,0 +1,10 @@
+package com.student.fahrtenbuchapp.asyntask;
+
+/**
+ * Created by Mojdeh.aaz on 29.11.2016.
+ */
+
+public
+class UsersAsynctask
+{
+}
---
+++ app/src/main/java/com/student/fahrtenbuchapp/database/DbLogin.java
@@ -0,0 +1,10 @@
+package com.student.fahrtenbuchapp.database;
+
+/**
+ * Created by Mojdeh.aaz on 29.11.2016.
+ */
+
+public
+class DbLogin
+{
+}
